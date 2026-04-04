ca698e2851344b244c9d4c7196d9e506988b6dd1
Sumaiya176 <sumaiya.tr96@gmail.com>
2026-04-05 00:42:23 +0600

    url updated

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 import toast from 'react-hot-toast';
 
 //const API_BASE_URL = 'http://localhost:10000';
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:10000';
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'https://smart-inventory-xm1j.onrender.com';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
